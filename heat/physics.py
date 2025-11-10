b15be2d0d16f3bc82a4b12298460fc32a8e127d1
Saif Aqel <saifaqel3@gmail.com>
2025-11-10 19:12:40 +0100

    resistances bug fix for multible tubes

--- heat/physics.py
+++ heat/physics.py
@@ -1,6 +1,10 @@
 from math import pi, log
 from common.units import Q_
 
+def _nt(spec) -> float:
+    Nt = spec.get("tubes_number", None)
+    return (Nt.to("").magnitude if Nt is not None else 1.0)
+
 def fouling_resistances(spec: dict) -> tuple[Q_, Q_]:
     di = spec["inner_diameter"].to("m")
     do = spec["outer_diameter"].to("m")
@@ -12,10 +16,14 @@ def fouling_resistances(spec: dict) -> tuple[Q_, Q_]:
     Rfi = log(di/Di_new)/(2*pi*kfi)
     do_new = do + 2*tfo
     Rfo = log(do_new/do)/(2*pi*kfo)
-    return Rfi, Rfo
+
+    Nt = _nt(spec)
+    return (Rfi / Nt).to("K*m/W"), (Rfo / Nt).to("K*m/W")
 
 def wall_resistance(spec: dict) -> Q_:
     di = spec["inner_diameter"].to("m")
     do = spec["outer_diameter"].to("m")
     k  = spec["wall_k"].to("W/m/K")
-    return (log(do/di) / (2*pi*k)).to("K*m/W")
+    R = log(do/di) / (2*pi*k)
+    Nt = _nt(spec)
+    return (R / Nt).to("K*m/W")
